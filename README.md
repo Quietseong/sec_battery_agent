8674c2e749ab151a68ee15b0a8c11c76215ae76a
Quietseong <quietseong@user.noreply.github.com>
2026-03-22 19:05:41 +0900

    feat: Mirgrate to poetry->uv

---
+++ README.md
@@ -0,0 +1,1 @@
+# sec_battery_agent
---
+++ src/eval_pipeline/__init__.py
@@ -0,0 +1,7 @@
+"""이차전지 도메인 LLM 평가 파이프라인
+
+배터리 도메인 특화 모델의 성능을 자동으로 평가하기 위한 모듈.
+LLM-as-Judge 패턴으로 faithfulness, relevancy, coherence를 측정 
+"""
+
+__version__ = "0.1.0"
